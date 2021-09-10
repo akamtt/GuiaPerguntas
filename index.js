b55340520ae265a88b3cac164b861bf0024e6364
akamtt <theusluiz0@gmail.com>
2021-09-10 19:11:46 -0300

    Criacão e conexão com o banco de dados e aplicação de partials

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
+const connection = require('./database/database');
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+  })
+  .catch((msgErro) => {});
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//ROTAS
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -10,6 +23,15 @@ app.get('/', (req, res) => {
 app.get('/perguntar', (req, res) => {
   res.render('perguntar');
 });
+
+app.post('/salvarpergunta', (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send(
+    'Formulário Recebido! Título: ' + titulo + ' ' + 'Descrição:  ' + descricao,
+  );
+});
+
 app.listen(8080, () => {
   console.log('App rodando!');
 });
